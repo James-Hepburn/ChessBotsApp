c3e837f1e03c5caf51a4eeacd3661e2e8b37b0de
James Hepburn <jhepb059@uottawa.ca>
2026-02-25 16:27:17 -0500

    Initial Commit

---
+++ ChessBotsApp/ChessBotsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessBotsAppApp.swift
+//  ChessBotsApp
+//
+//  Created by James Hepburn on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ChessBotsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
